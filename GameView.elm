bb137f74c7e58e08f782faa8a95e676e12c4270a
Alex Neslusan <deadfoxygrandpa@gmail.com>
2014-09-10 03:24:29 +0800

    Add player movement

--- GameView.elm
+++ GameView.elm
@@ -32,15 +32,20 @@ tile t =
 player : Form
 player = filled red <| circle (toFloat xScale / 2)
 
+enemy : Form
+enemy = filled green <| circle (toFloat xScale / 2)
+
 display : GameModel.State -> Element
 display state =
     let (w, h) = (state.level.size.width * xScale, state.level.size.height * yScale)
         xOffset n = ((toFloat n) - (toFloat state.level.size.width) / 2) * (toFloat xScale)
         yOffset n = ((toFloat n) - (toFloat state.level.size.height) / 2) * (toFloat yScale)
         row n tiles = map (\(n', t) -> move (xOffset n', yOffset n) <| tile t) <| zip [0..state.level.size.width - 1] tiles
-        pl = (xOffset state.player.location.x, yOffset state.player.location.y)
+        pl = (xOffset state.player.location.x, 0 - yOffset (state.player.location.y + 1))
         player' = move pl player
+        el = (xOffset state.enemy.location.x, 0 - yOffset (state.enemy.location.y + 1))
+        enemy' = move el enemy
         grid = Grid.toList state.level
         bg = collage w h <|(concatMap (\(n, t) -> row n t) <| zip [0..state.level.size.height - 1] grid)
-        pg = collage w h [player]
-    in  flow down [bg, pg, collage w h [moveX -120 player], asText state.player, asText pl]
+        pg = collage w h [player', enemy']
+    in  flow down [layers [bg, pg], flow down <| map asText state.log]

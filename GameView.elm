61907be8bbf4763d5e8687c0fb8a0aaa6043e18f
Alex Neslusan <deadfoxygrandpa@gmail.com>
2014-09-18 23:36:53 +0800

    Fix collage layout

--- GameView.elm
+++ GameView.elm
@@ -37,15 +37,19 @@ enemy = circle (toFloat xScale / 2) |> filled green
 
 display : GameModel.State -> Element
 display state =
-    let (w, h) = (state.level.size.width * xScale, state.level.size.height * yScale)
-        xOffset n = ((toFloat n) - (toFloat state.level.size.width) / 2) * (toFloat xScale)
-        yOffset n = ((toFloat n) - (toFloat state.level.size.height) / 2) * (toFloat yScale)
-        row n tiles = map (\(n', t) -> move (xOffset n', yOffset n) <| tile t) <| zip [0..state.level.size.width - 1] tiles
-        pl = (xOffset state.player.location.x, 0 - yOffset (state.player.location.y + 1))
-        player' = move pl player
-        el = (xOffset state.enemy.location.x, 0 - yOffset (state.enemy.location.y + 1))
-        enemy' = move el enemy
-        grid = Grid.toList state.level
-        bg = collage w h <|(concatMap (\(n, t) -> row n t) <| zip [0..state.level.size.height - 1] grid)
-        pg = collage w h [player', enemy']
-    in  flow down [layers [bg, pg], flow down <| map asText state.log]
+    let (w, h)         = (state.level.size.width * xScale, state.level.size.height * yScale)
+        xOffset n      = ((toFloat n) - (toFloat state.level.size.width) / 2) * (toFloat xScale)
+        yOffset n      = ((toFloat n) - (toFloat state.level.size.height) / 2) * (toFloat yScale)
+        row n tiles    = let tiles' = zip [0..state.level.size.width - 1] tiles
+                             makeTile (n', t) = move (xOffset n', yOffset n) <| tile t
+                         in  map makeTile tiles'
+        playerLocation = (xOffset state.player.location.x, 0 - yOffset (state.player.location.y + 1))
+        player'        = move playerLocation player
+        enemyLocation  = (xOffset state.enemy.location.x, 0 - yOffset (state.enemy.location.y + 1))
+        enemy'         = move enemyLocation enemy
+        grid           = Grid.toList state.level
+        bg             = collage (w + xScale) (h + yScale) <|(concatMap (\(n, t) -> row n t) <| zip [0..state.level.size.height - 1] grid)
+        pg             = collage (w + xScale) (h + yScale) [player', enemy']
+    in  flow down [ layers [bg, pg]
+                  , flow down <| map (toText >> monospace >> centered) state.log
+                  ]
